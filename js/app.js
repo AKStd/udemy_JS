45a7f8bfb7646f7cde364eafa32ffdebe0d0e778
Aleksandr <karpov.a1exandr@yandex.ru>
2021-02-04 20:57:17 +0300

    lesson 15 done

--- js/app.js
+++ js/app.js
@@ -11,9 +11,30 @@ let personalMovieDB = {
     privat : false
 };
 
+
+function isItNull(strg) {
+    if (strg == null) {
+        return false;
+    } else if (strg == ''){
+        return false;
+    } else if (strg.lenght >= 50) {
+        return false;
+    } else {
+        return true;
+    }
+    
+}
+
 for  (let i = 0; i < 2; i++) {
     let lastMovieTitle = prompt('последний фильм?' , ''),
         lastMovieRate = prompt(`как оценишь ${lastMovieTitle}?` ,'');
+
+    let answers = [lastMovieTitle, lastMovieRate];
+    console.log(answers);
+
+    for (let j = 0; j< answers.lenght; j++) {
+        console.log(answers[j]);
+    }
 
     personalMovieDB.movies[lastMovieTitle] = lastMovieRate;
 
