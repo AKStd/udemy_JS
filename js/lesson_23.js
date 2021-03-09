0803837944d8c151801f8f23e1458e4d9e08ce8e
Aleksandr <karpov.a1exandr@yandex.ru>
2021-03-09 22:35:40 +0300

    lesson 23 done

--- js/lesson_23.js
+++ js/lesson_23.js
@@ -1,4 +1,40 @@
 "use strict";
 
 let str = 'some';
-let strObject = String(str);
+let strObject = new String(str);
+
+
+// смотри наследование
+console.dir([1,2,3,4,5,6]);
+
+
+// absract object
+const soldier = {
+    name: 'unnamed',
+    health: 100,
+    armor: 100,
+    sayHello: function(){
+        return `say hello to ${this.name}`;
+    }
+};
+
+// создаем экземпляры объекта  
+// bad case
+const Jax = {
+    name: 'Jax',
+    health:400
+};
+
+Jax.__proto__ = soldier;
+
+console.log(Jax);
+// but
+console.log(Jax.armor);
+
+console.log(Jax.sayHello());
+
+
+const Nick = {};
+Object.setPrototypeOf(Nick, soldier);
+// or
+const John = Object.create(soldier);
